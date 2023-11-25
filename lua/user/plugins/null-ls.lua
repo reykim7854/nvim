39f8584cd43c3d76ed2b302590cd72d70fb34389
reykim7854 <reykim7124@proton.me>
2023-11-26 06:46:24 +0700

    feat: use none-ls as null-ls replacement

--- lua/user/plugins/null-ls.lua
+++ lua/user/plugins/null-ls.lua
@@ -1,5 +1,5 @@
 return {
-  "jose-elias-alvarez/null-ls.nvim",
+  "nvimtools/none-ls.nvim",
   opts = function(_, config)
     -- config variable is the default configuration table for the setup function call
     local null_ls = require "null-ls"
@@ -10,7 +10,9 @@ return {
     config.sources = {
       -- Set a formatter
       null_ls.builtins.formatting.stylua,
-      null_ls.builtins.formatting.prettier,
+      null_ls.builtins.formatting.prettier.with {
+        extra_filetypes = { "svelte" },
+      },
       null_ls.builtins.formatting.eslint,
     }
     return config -- return final config table
